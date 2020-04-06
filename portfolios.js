10c942d1d4004bcc9f3d9f4e7774dc5213a5566e
Engel Mallen <emallen@uarts.edu>
2020-04-05 22:50:52 -0400

    gallery now working

--- portfolios.js
+++ portfolios.js
@@ -1,1 +1,76 @@
-var x= 0;
+let modal = document.getElementById("modal")
+let butHire = document.getElementById("butHire")
+let modalForm = document.getElementById("modalForm")
+
+let change = false;
+let timerSwitch = true;
+
+			//Modal:
+function modaling() {
+	if (change === false)
+		{
+		modal.style.transition = "3s";	
+		modal.style.marginTop = "5%";	
+		change = true
+		}
+		else	{	
+				modal.style.marginTop = "-50%";
+				change = false
+				clearAll(modalForm)}
+	timerSwitch = false;}
+
+function modalTimer() {
+		setTimeout(function () {
+			modaling()
+		},3000)}
+
+function clearAll(currentForm){currentForm.reset()}
+
+butHire.addEventListener("click", function(){
+	modaling()			
+	console.log('butHire clicked')})
+
+buttonClose.addEventListener("click", function(){
+	modaling();
+	console.log('butClose clicked')
+})
+
+
+window.onload = function() 
+{
+	if (timerSwitch = true)
+	{
+		modalTimer();
+		timerSwitch = false;
+	}
+}
+
+			//Gallery
+
+let galleryImgs = document.getElementById("galleryImgs")
+let galleryLft = document.getElementById("galleryLft")
+let galleryRgt = document.getElementById("galleryRgt")
+
+let imgIndex = 1;
+
+function status() {
+ 	console.log(imgIndex)
+}
+
+function galMove() {
+	if (imgIndex > 8) {imgIndex = 0}
+	if (imgIndex < 1) {imgIndex = 8}
+	galleryImgs.style.background = "url(img/gallery/imgGal"+imgIndex+".jpg) no-repeat center";
+	galleryImgs.style.backgroundSize =  "contain";
+	status()
+}galMove()
+
+galleryRgt.addEventListener('click', function () {
+	imgIndex += 1;
+	galMove()
+})
+
+galleryLft.addEventListener('click', function () {
+	imgIndex -= 1;
+	galMove()
+})
